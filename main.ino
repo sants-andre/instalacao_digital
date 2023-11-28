ed38642867654888915edabd96f65f1c650429f9
edualfo <eduardo.abreulfo@gmail.com>
2023-11-28 10:05:33 -0300

    vazia

--- main.ino
+++ main.ino
@@ -1,1 +1,9 @@
+void setup() {
+  // This function runs once when the Arduino board is powered on or reset.
+  // It is used to initialize variables, pin modes, libraries, etc.
+}
 
+void loop() {
+  // This function runs repeatedly after the setup() function has finished.
+  // It is used to implement the main logic of your Arduino program.
+}
