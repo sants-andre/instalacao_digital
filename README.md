f7431b0e8744cbfd2c526e39fd600287cf31aa05
edualfo <eduardo.abreulfo@gmail.com>
2023-11-30 11:30:28 -0300

    arranjando os dados

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-# instalacao_digital
+# Instalação Digital
+
+- André Santos
+- Cézar Menotti
+- Matheus
+- Eduardo Abreu
+- Salamon
+-
+-
+
+## Índices
+
+<p>
+Foi criada uma "matriz 3d" para acomodar os dados que serão acessados.
+No caso, uma tabela/matriz 2d para cada rótulo, o que equivale à matriz 3d citada,
+sendo a quantidade de rótulos a "profundidade" da matriz.
+</p>
+
+<div style="display: flex;">
+  <div>
+    <h3>Rótulos</h3>
+    0: Atropelamentos <br>
+    1: Colisões<br>
+    2: Capotamentos<br>
+  </div>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <div>
+    <h3>Cidades</h3>
+    0: Vitória<br>
+    1: Vila Velha<br>
+    2: Serra<br>
+    3: Cariacica<br>
+    4: *BRASIL*<br>
+  </div>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <div>
+    <h3>Anos</h3>
+    0: 2019<br>
+    1: 2020<br>
+    2: 2021<br>
+    3: 2022<br>
+  </div>
+</div>
+
+<br>
+
+
+<table>
+<h3>*vezes* o nº de rótulos</h3>
+<tbody>
+  <tr>
+    <td>Cidade/Ano</td>
+    <td>2019</td>
+    <td>2020</td>
+    <td>2021</td>
+    <td>2023</td>
+  </tr>
+  <tr>
+    <td>Vitória</td>
+    <td>*</td>
+    <td>*</td>
+    <td>*</td>
+    <td>*</td>
+  </tr>
+  <tr>
+    <td>Vila Velha</td>
+    <td>*</td>
+    <td>*</td>
+    <td>*</td>
+    <td>*</td>
+  </tr>
+  <tr>
+    <td>Serra</td>
+    <td>*</td>
+    <td>*</td>
+    <td>*</td>
+    <td>*</td>
+  </tr>
+  <tr>
+    <td>Cariacica</td>
+    <td>*</td>
+    <td>*</td>
+    <td>*</td>
+    <td>*</td>
+  </tr>
+</tbody>
+</table>
--- main.ino
+++ main.ino
@@ -22,76 +22,51 @@
 
 // Inicialização de cada sensor de ultrassom, para passagem dos anos
 Ultrasonic
-  ultrasonic_0(PINO_TRIGGER_0, PINO_ECHO_0),
-  ultrasonic_1(PINO_TRIGGER_1, PINO_ECHO_1),
-  ultrasonic_2(PINO_TRIGGER_2, PINO_ECHO_2),
-  ultrasonic_3(PINO_TRIGGER_3, PINO_ECHO_3);
+ultrasonic_0(PINO_TRIGGER_0, PINO_ECHO_0),
+ultrasonic_1(PINO_TRIGGER_1, PINO_ECHO_1),
+ultrasonic_2(PINO_TRIGGER_2, PINO_ECHO_2),
+ultrasonic_3(PINO_TRIGGER_3, PINO_ECHO_3);
 
 // Indices
-int
-  i=0,
-  j=0,
-  k=0;
+int i=0, j=0, k=0;
 
-/* Anos
-  * 0: 2014
-  * 1: 2015
-  * 2: 2016
-  * 3: 2017
-  */
- 
-/* Cidades
-  * 0: Vitória
-  * 1: Vila Velha
-  * 2: Serra
-  * 3: Cariacica
-  * 4: BRASIL
-  */
-
-/* Rótulos
-  * 0: Atropelamentos
-  * 1: Colisões
-  * 2: Capotamentos
-  */
-
-int dados
-  [QTD_ROTULOS]
-  [QTD_CIDADES + 1] // +1 para o Brasil
-  [QTD_ANOS] = {
-    // Atropelamentos
-    { 
-      // 2019, 2020, 2021, 2022
-      { 14, 27, 42, 51 }, // Vitória
-      { 14, 27, 42, 51 }, // Vila Velha
-      { 14, 27, 42, 51 }, // Serra
-      { 14, 27, 42, 51 }, // Cariacica
-      { 14, 27, 42, 51 }  // BRASIL
-    },
-    // Internacoes
-    { 
-      // 2019, 2020, 2021, 2022
-      { 14, 27, 42, 51 }, // Vitória
-      { 14, 27, 42, 51 }, // Vila Velha
-      { 14, 27, 42, 51 }, // Serra
-      { 14, 27, 42, 51 }, // Cariacica
-      { 14, 27, 42, 51 }  // BRASIL
-    },
-    // Gastos Hospitalares
-    { 
-      // 2019, 2020, 2021, 2022
-      { 14, 27, 42, 51 }, // Vitória
-      { 14, 27, 42, 51 }, // Vila Velha
-      { 14, 27, 42, 51 }, // Serra
-      { 14, 27, 42, 51 }, // Cariacica
-      { 14, 27, 42, 51 }  // BRASIL
-    }
-  };
-
+const int DADOS
+[QTD_ROTULOS]
+[QTD_CIDADES + 1] // +1 para o Brasil
+[QTD_ANOS] = {
+  // Atropelamentos
+  { 
+    // 2019, 2020, 2021, 2022
+    { 14, 27, 42, 51 }, // Vitória
+    { 14, 27, 42, 51 }, // Vila Velha
+    { 14, 27, 42, 51 }, // Serra
+    { 14, 27, 42, 51 }, // Cariacica
+    { 14, 27, 42, 51 }  // BRASIL
+  },
+  // Internacoes
+  { 
+    // 2019, 2020, 2021, 2022
+    { 14, 27, 42, 51 }, // Vitória
+    { 14, 27, 42, 51 }, // Vila Velha
+    { 14, 27, 42, 51 }, // Serra
+    { 14, 27, 42, 51 }, // Cariacica
+    { 14, 27, 42, 51 }  // BRASIL
+  },
+  // Gastos Hospitalares
+  { 
+    // 2019, 2020, 2021, 2022
+    { 14, 27, 42, 51 }, // Vitória
+    { 14, 27, 42, 51 }, // Vila Velha
+    { 14, 27, 42, 51 }, // Serra
+    { 14, 27, 42, 51 }, // Cariacica
+    { 14, 27, 42, 51 }  // BRASIL
+  }
+};
 
 void setup() {
-  ;
+  Serial.begin(9600);
 }
 
 void loop() {
-  ;
+  delay(10);
 }
